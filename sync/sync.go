2e4472cc5abd06c4d692672c0c6cc104be175840
Pavel Perminov <start@perminov.im>
2024-11-16 21:27:51 +0200

    once added

--- sync/sync.go
+++ sync/sync.go
@@ -23,3 +23,10 @@ func GetSemaphore(amount int) (func(), func()) {
 	s := semaphore.NewWeighted(int64(amount))
 	return func() { s.Acquire(context.Background(), 1) }, func() { s.Release(1) }
 }
+
+func GetOnce() func(func()) {
+	var once sync.Once
+	return func(f func()) {
+		once.Do(f)
+	}
+}
